db19229e7a79c0014c3f94afed6c59a681b89cb1
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-05-14 15:08:31 +0700

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Categories from "../components/homepage-components/Categories";
 import ProductListHome from "../components/homepage-components/ProductListHome";
 import Subscribe from "../components/homepage-components/Subscribe";
 import HomeBanner from "../components/homepage-components/HomeBanner";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Popup from "../components/Popup/Popup";
 export default function HomePage() {
   const isShowPopup = useSelector((state) => state.popup.isShow);
